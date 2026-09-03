b7f01b13e0297a0f8c2085d206bdfd1442a5d3c0
Maphuti-gif <maphutimoviousteffo@gmail.com>
2026-09-03 10:17:34 +0200

    Update repository settings in settings.gradle.kts

--- WIZPERLY/settings.gradle.kts
+++ WIZPERLY/settings.gradle.kts
@@ -7,12 +7,10 @@ pluginManagement {
     }
 }
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
     repositories {
         google()
         mavenCentral()
         maven { url = uri("https://jitpack.io") }
-        maven { url = uri("https://download.jetbrains.com/kotlin/native/builds/releases/1.9.22/windows-x86_64") }
         maven { url = uri("https://download.jetbrains.com/kotlin/native/builds") }
     }
 }
